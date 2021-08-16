9b1e7ae1ce70a697bb531234536fe6d9695c27ba
shasibhusan <shjena@adobe.com>
2021-08-16 15:21:32 +0530

    commit draft

---
+++ AreaOFcircle.py
@@ -0,0 +1,6 @@
+def areaCircle(radious):
+    pi = 3.14
+    area = 3.14 *(radious *radious)
+    return area
+
+print("Area is %.6f " % areaCircle(5) )
---
+++ aws/s3/insertFileToS3.py
@@ -0,0 +1,21 @@
+import boto
+from boto.s3.key import Key
+
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+END_POINT = 'Global'                          # eg. us-east-1
+S3_HOST = 's3.ap-south-1.amazonaws.com'                            # eg. s3.us-east-1.amazonaws.com
+BUCKET_NAME = 'shasi-s3-bucket'
+FILENAME = 'upload.txt'
+UPLOADED_FILENAME = 'dumps/upload.txt'
+# include folders in file path. If it doesn't exist, it will be created
+
+s3 = boto.s3.connect_to_region(END_POINT,
+                           aws_access_key_id=AWS_ACCESS_KEY_ID,
+                           aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                           host=S3_HOST)
+
+bucket = s3.get_bucket(BUCKET_NAME)
+k = Key(bucket)
+k.key = UPLOADED_FILENAME
+k.set_contents_from_filename(FILENAME)
